f52515d8f1727a30532e552af9c8a23f1e54ccd5
James Taber <JATaber@fullsail.edu>
2018-03-01 15:35:38 -0600

    working out bugs from following video

--- APP1/app1/routes/index.js
+++ APP1/app1/routes/index.js
@@ -5,20 +5,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('index', {
               title: 'Home',
-              navitems: [
-              {link: '/', content: 'Home'},
-              {link: '/users', content: 'Users'}
-              ]});
+              });
 });
 
 router.get('/users', function(req, res, next) {
     res.render('users', {
               title: 'Users',
               users:[{"Name":"James", "Lastname":"Taber"}],
-              navitems: [
-              {link: '/', content: 'Home'},
-              {link: '/users', content: 'Users'}
-              ]});
+              });
 });
 
 module.exports = router;
