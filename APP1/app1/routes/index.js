c62977e927c372fb71b88da9fcf42cace5a98ffc
James Taber <JATaber@fullsail.edu>
2018-03-01 15:40:15 -0600

    working out bugs

--- APP1/app1/routes/index.js
+++ APP1/app1/routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    res.locals.navitems= [{link:'/', content:'Home'},
+                          {link:'/users', content:'Users'}];
+                          
     res.render('index', {
               title: 'Home',
               });
@@ -12,7 +15,10 @@ router.get('/users', function(req, res, next) {
     res.render('users', {
               title: 'Users',
               users:[{"Name":"James", "Lastname":"Taber"}],
-              });
+              navitems: [
+              {link: '/', content: 'Home'},
+              {link: '/users', content: 'Users'}
+              ]});
 });
 
 module.exports = router;
